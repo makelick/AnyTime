5f4dcc47cb9a662adf36774bcf252014652c8e34
makelick <vi.neshcheret@gmail.com>
2023-12-07 21:47:16 +0200

    Add dependency and create xml for LoginSplashFragment

---
+++ src/app/src/main/java/com/makelick/anytime/BaseFragment.kt
@@ -0,0 +1,23 @@
+package com.makelick.anytime
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB : ViewBinding>(
+    private val layoutInflater: (inflater: LayoutInflater) -> VB
+) : Fragment() {
+
+    protected lateinit var binding: VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = layoutInflater.invoke(inflater)
+        return binding.root
+    }
+}
---
+++ src/app/src/main/java/com/makelick/anytime/LoginSplashFragment.kt
@@ -0,0 +1,15 @@
+package com.makelick.anytime
+
+import android.os.Bundle
+import android.view.View
+import com.makelick.anytime.databinding.FragmentLoginSplashBinding
+
+class LoginSplashFragment :
+    BaseFragment<FragmentLoginSplashBinding>(FragmentLoginSplashBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
